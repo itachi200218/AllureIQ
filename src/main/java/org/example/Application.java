ad2ef3d0b83d00da9beb48da2e75019b65feddaa
chetan <chetanyaadepu@gmail.comgit config --global user.email chetanyaadepu@gmail.com>
2025-11-10 02:28:14 +0530

    New Render

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -8,13 +8,24 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
     }
 
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> webServerFactoryCustomizer() {
-        return factory -> factory.setPort(8081);
+        return factory -> {
+            // Render provides a dynamic port using the PORT environment variable.
+            String port = System.getenv("PORT");
+
+            if (port != null) {
+                // When deployed on Render
+                factory.setPort(Integer.parseInt(port));
+            } else {
+                // When running locally
+                factory.setPort(8081);
+            }
+        };
     }
 }
